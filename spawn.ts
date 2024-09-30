d17f621ad73ac734c559b59656d7bb07f0de719b
Charles Lowell <cowboyd@frontside.com>
2024-09-30 07:41:43 -0500

    remove unused contexts

--- spawn.ts
+++ spawn.ts
@@ -1,8 +1,5 @@
 import type { Operation, Task } from "./types.ts";
-import { createContext } from "./context.ts";
 import { useScope } from "./scope.ts";
-
-//export const Tasks = createContext<Set<Task<unknown>>>("@effection/tasks");
 
 export function spawn<T>(op: () => Operation<T>): Operation<Task<T>> {
   return {
